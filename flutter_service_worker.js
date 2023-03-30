737233b5a3fd0a151f6534d6fc78a7bb3093e390
mohamadahmadisadr <m.ahmadi@solutions-apps.de>
2023-03-30 21:43:20 +0330

    add new features

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "29623f2aec23ca8fdec00bdf0143aa5b",
-"index.html": "f22cc421899cc6f85ec5a5c34511fa33",
-"/": "f22cc421899cc6f85ec5a5c34511fa33",
+"index.html": "b7d49d3e67b60e792b8c82f3c40ab750",
+"/": "b7d49d3e67b60e792b8c82f3c40ab750",
 "firebase-messaging-sw.js": "96b418b2375b7b73286be344adcfb0a3",
-"main.dart.js": "0509b0a98a519555acd7af5551f118da",
+"main.dart.js": "18deb8b81b0b014bfa6acacdff50713d",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "dc23764c094663e20a1e01b3687c6e23",
 "icons/Icon-192.png": "e57297a1c4171c0811e9278714aff10f",
